3eeb5cf3f8ca734bbcc31599a8e6bc284574feb1
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2023-12-14 18:31:57 -0500

    add theme.js

---
+++ src/theme/theme.js
@@ -0,0 +1,26 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const fonts = { heading: "Roboto, sans-serif", body: "Roboto, sans-serif" };
+const Button = {
+  variants: {
+    outline: {
+      borderRadius: "0",
+      textTransform: "uppercase",
+      fontWeight: "light",
+      letterSpacing: "1px",
+      color: "white",
+      _hover: { color: "black" },
+    },
+  },
+};
+const Heading = {
+  variants: {
+    banner: {
+      textTransform: "uppercase",
+      fontWeight: "light",
+      letterSpacing: "5px",
+      color: "white",
+    },
+  },
+};
+export const theme = extendTheme({ fonts, components: { Button, Heading } });
